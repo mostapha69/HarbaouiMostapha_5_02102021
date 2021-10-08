c05f0277c679a5b4486433ff3994581759f12f34
mostapha69 <mostapha.harbaoui@gmail.com>
2021-10-08 19:10:09 +0200

    probleme fetch réglé + ajout prix

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,5 @@
-const section = document.querySelector('item');
-const template = document.querySelector('template__item');
+const section = document.querySelector('.item');
+const template = document.querySelector('.template');
 
 var searchParams = new URLSearchParams(document.location.search.substring(1));
 console.log(searchParams)
@@ -12,23 +12,26 @@ console.log(id)
 
 fetch('http://localhost:3000/api/products/'+id)
 .then((res) => res.json())
-.then (product => {
-    
-    //   console.log(product)
+.then((product) => {
      
-      const clone = document.importNode(template.content, true);
-     let baliseImg= clone.querySelector('img');
+    const clone = document.importNode(template.content, true);
+     let baliseImg = clone.querySelector('img');
      let baliseH1 = clone.querySelector('h1'); 
-     let balisePPRIX= clone.querySelector('#price');
+     let balisePPRIX= clone.querySelector('.price');
+     let balisePDescription = clone.querySelector('#description');
+     let baliseSelectColors = clone.querySelector('#colors');
      baliseImg.setAttribute('src',product.imageUrl);
      baliseImg.setAttribute('alt',product.altTxt);
      balisePPRIX.textContent = product.price;
      baliseH1.textContent = product.name;
-    // baliseA.setAttribute('href','product.html?id='+product._id);
-    section.appendChild(clone);
+     balisePDescription.textContent = product.description;
+     baliseSelectColors.textContent = product.colors;
 
+    // baliseA.setAttribute('href','product.html?id='+product._id);
+     section.appendChild(clone);
+      
 })
 
 
-    // console.log(product)
+    //  console.log(product)
 
